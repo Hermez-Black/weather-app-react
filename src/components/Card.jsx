a973cc98e1cfdb3fd5ed94217e35d8677006e344
Braulio <braulio@apurata.com>
2023-03-08 19:51:25 -0600

    fix responsive

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
 export default function Card({ data, degreesNumber }) {
+
+  const icon = data?.weather[0]?.icon;
+
+
   return (
     <div className='card'>
-      <img className='item1' src={`/public/${data?.weather[0]?.icon}.svg`} alt="icon weather api" />
+      <img className='item1' src={`/public/${icon}.svg`} alt="icon weather api" />
       <p className='item2'>{degreesNumber}°</p>
       <div className='item3'>Wind: {data?.wind?.speed}<br/>Clouds: {data?.clouds?.all}<br/>Pressure: {data?.main?.pressure}</div>
       <div className='item4'>{data?.weather[0]?.description}</div>
