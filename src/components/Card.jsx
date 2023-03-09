e10a16f1e1ef50a50eef347d21be3e909e08a142
Braulio <braulio@apurata.com>
2023-03-09 00:23:27 -0600

    shows icon on prd

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,9 +8,11 @@ export default function Card({ data, degreesNumber}) {
     return iconArray;
   }
 
+  let nameIcon = strNameIcon()?.join("");
+
   return (
     <div className='card'>
-      <img className='item1' src={`/public/${strNameIcon()?.join("")}n.svg`} alt="icon weather api" />
+      <img className='item1' src={`/public/${ nameIcon ? nameIcon : "02"}n.svg`} alt="icon weather api" />
       <p className='item2'>{degreesNumber}°</p>
       <div className='item3'>Wind: {data?.wind?.speed}<br/>Clouds: {data?.clouds?.all}<br/>Pressure: {data?.main?.pressure}</div>
       <div className='item4'>{data?.weather[0]?.description}</div>
